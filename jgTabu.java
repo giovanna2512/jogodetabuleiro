df733457bfdb2368247aa360d898048a69ca0a84
Gabriel Silva Gomes <gabriel.silvagomes2006@gmail.com>
2025-05-19 22:19:40 -0300

    INSTRUÇÕES (INCOMPLETAS)
    add função "instruções"

--- jgTabu.java
+++ jgTabu.java
@@ -1,7 +1,15 @@
 import java.util.Scanner;
 import java.util.Random;
 
-public class jgTabu {
+//ideias:
+//- tabuleiro de terror
+//- corrida
+//- circuito aberto
+//- charadas em cada casa (penalidade personalizada por charada)
+//- "NPC" que de *2 em 2 rodadas diminui o numero de casas
+//- mensagem de morte personalizada
+
+public class Main {
     public static int exibirMenu() {
         Scanner input = new Scanner(System.in);
         int escolha;
@@ -30,6 +38,11 @@ public class jgTabu {
         int escolha2 = input.nextInt();
 
         return escolha2;
+    }
+    public static void exibirInstrucoes() {
+        System.out.println("As almas aqui presentes devem fugir do umbral antes que sejam encontradas pelo carrasco.");
+        System.out.println("COMO JOGAR: \n" + "1. Cada turno, o jogador ativo lança o dado de 3 lados e avança seu peão o número de casas correspondente.\n" +
+                "2. Após o movimento do jogador, o Carrasco avança 1 casa a cada turno do conjunto de jogadores (exemplo: se houver 4 jogadores, a cada 4 turnos o carrasco avança 1 casa).\n");
     }
 
     public static void main(String[] args) {
